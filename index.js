9807a9b8d125eb20550a62ca806e13875f4ed848
shova29 <halima2996@gmail.com>
2022-05-09 21:00:32 +0600

    delete item api added

--- index.js
+++ index.js
@@ -61,6 +61,14 @@ async function run() {
       const result = await inventoryCollection.insertOne(inventoryItem);
       res.send(result);
     });
+
+    //DELETE
+    app.delete("/inventory/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await inventoryCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
